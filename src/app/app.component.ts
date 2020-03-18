802cb4687e7579cf47160463b68ad608be3e8568
max-student <max-student@maxtrain.com>
2020-03-18 16:32:12 -0400

    end of day 3/18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world-angular';
+  title = 'Angular 9';
+  name:string = "no name yet";
+  color:string = "black";
+  isHidden:boolean = false;
+
+  setName():void {
+    this.name = "Parsley 冬";
+  }
+  setColor():void {
+    this.color = (this.color == "black") ? "violet" : "black";
+  }
+  flipHidden():void {
+    this.isHidden = !this.isHidden;
+  }
 }
